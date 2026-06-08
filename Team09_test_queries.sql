cb7b227b0d83e87231e767f08bea7722aca8854f
albarracine <albarracine@mymacewan.ca>
2026-06-08 18:53:31 +0000

    small adjustments

--- Team09_test_queries.sql
+++ Team09_test_queries.sql
@@ -9,27 +9,32 @@ SELECT People.firstName||' '||People.lastName as Customer_Name, Customer.*
 FROM People, Customer
 WHERE (People.peopleID = Customer.peopleID)
 
+
 /* Show all movies with type and copy/available data. */
 SELECT M.movieName, STRING_AGG(G.genreString, ', ') as Genre, M.copiesAvailable
 FROM Movie as M, Genre as G
 WHERE M.movieID = G.MovieID
 GROUP BY M.movieName, M.copiesAvailable
 
+
 /* Show a selected customer's queue. */
 SELECT movieName, queuePosition
 FROM Movie, MovieQueue, JoinQueue
 WHERE (JoinQueue.FK_customerID = 'C-000000001') and (JoinQueue.queueID = MovieQueue.queueID) and (MovieQueue.FK_movieID = Movie.movieID)
+
 
 /* Show active rentals. */
 SELECT movieName, issueDate, dueDate
 FROM Orders, Movie
 WHERE (Movie.movieID = Orders.movieID)
 
+
 /* Show rental history for a selected customer. */
 SELECT People.firstName||' '||People.lastName as Customer_Name, Movie.movieName, Orders.issueDate, Orders.dueDate, Orders.returnDate, OrderContext.orderStatus
 FROM People, Customer, Orders, Movie, OrderContext
 WHERE (Orders.customerID = Customer.customerID) and (Customer.peopleID = People.peopleID) and (Orders.movieID = Movie.movieID) and (Orders.contextID = OrderContext.contextID)
     and (Orders.customerID = 'C-000000001')
+
 
 /* Show movie availability. */
 SELECT Movie.movieName, Movie.copiesAvailable
